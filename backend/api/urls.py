b3037ab36b104722b816a9f7427d2fc86b6361f0
edcedcedcedc <ranogaet@gmail.com>
2025-06-18 20:53:19 +0300

    Register and login views and routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,4 @@
+from .views import register, login
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -7,4 +8,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/register/", register, name="register"),
+    path("api/login/", login, name="login"),
 ]
