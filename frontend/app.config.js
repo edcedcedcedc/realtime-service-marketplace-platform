c5df4690762f7f99da0cf9fbd1d8eb10600992b0
alexsad95 <aleksandrsadchikov.17@gmail.com>
2025-07-11 00:12:07 +0300

    (fix) only app.comfig.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,23 +1,41 @@
 import "dotenv/config";
 
-export default () => ({
+export default {
   expo: {
-    name: "yyourappname",
-    slug: "your-app",
+    name: "Symmetrical Engine",
+    slug: "symmetrical-engine",
     sdkVersion: "53.0.0",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      bundleIdentifier: "com.mycompany.myapp",
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
     android: {
       package: "com.mycompany.myapp",
+      edgeToEdgeEnabled: true,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
       config: {
         googleMaps: {
           apiKey: process.env.GOOGLE_MAPS_API_KEY,
         },
       },
     },
-    ios: {
-      bundleIdentifier: "com.mycompany.myapp",
-      config: {
-        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-      },
+    web: {
+      favicon: "./assets/favicon.png",
     },
   },
-});
+};
